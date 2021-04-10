175bfbf95fe87a2ef92b64d94eda51f1028bbbf0
Fr0zT-code <69401786+Fr0zT-code@users.noreply.github.com>
2021-04-10 17:32:08 +0200

    d

--- commands/utility/help.js
+++ commands/utility/help.js
@@ -1,4 +1,4 @@
-const  { MessageEmbed } = require('discord.js');
+const { MessageEmbed } = require('discord.js');
 const { Menu } = require('discord.js-menu');
 
 module.exports = {
@@ -9,36 +9,55 @@ module.exports = {
             {
                 name: 'main',
                 content: new MessageEmbed()
-                .setTitle('Welcome')
-                .setFooter('Page 1/3')
+                    .setTitle('Welcome')
+                    .setFooter('Page 1/3')
+                    .addFields(
+                        { name: 'Thank you', value: 'Thank you for using the Dev Com bot, this means a lot to its developers' },
+                        { name: 'Commands', value: 'You can find the commands page by reacting with ▶' }
+                    )
                 ,
-                reactions:{
-                '▶': "next"
+                reactions: {
+                    '▶': "next"
                 }
-                },
-            {
-                name: "otherPage",
-                content: new MessageEmbed()
-                .setTitle('Commands')
-                .setFooter('Page 2/3')
-                ,
-                reactions:{  
-                '◀': 'previous', 
-                '▶': "next"
-            }
             },
             {
                 name: "otherPage",
                 content: new MessageEmbed()
-                .setTitle('Information')
-                .setFooter('Page 3/3')
+                    .setTitle('Commands')
+                    .addFields(
+                        { name: 'Fun commands', value: 'Fun commands will be displayed underneath' },
+                        { name: '!add', value: 'Multiply two numbers' },
+                        { name: '!avatar', value: 'Shows the tagged users avatar. If you dont tag a user it will show yours' },
+                        { name: '!meme', value: 'Shows a random meme from the meme subreddit' },
+                        { name: 'Moderation commands', value: 'Moderation commands will be displayed underneath' },
+                        { name: '!mute', value: 'Mutes a user' },
+                        { name: '!unmute', value: 'Unmutes a user' },
+                        { name: 'Utility commands', value: 'Utility commands will be displayed underneath' },
+                        { name: '!developer', value: 'Gives credits to who developed the bot' },
+                        { name: '!embed', value: 'Makes it so you can embed some text' },
+                        { name: '!help', value: 'Shows the current command' },
+                        { name: '!ping', value: 'Shows the bots ping' },
+                        { name: '!serverinfo', value: 'Shows the serverinfo' }
+                    )
+                    .setFooter('Page 2/3')
                 ,
-                reactions:{  
-                '◀': 'previous'
-            }
+                reactions: {
+                    '◀': 'previous',
+                    '▶': "next"
+                }
             },
-            ], 300000)
-                helpMenu.start()
+            {
+                name: "otherPage",
+                content: new MessageEmbed()
+                    .setTitle('Information')
+                    .setFooter('Page 3/3')
+                ,
+                reactions: {
+                    '◀': 'previous'
+                }
+            },
+        ], 300000)
+        helpMenu.start()
     },
 }
 
