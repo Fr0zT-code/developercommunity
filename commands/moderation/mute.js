46cc8f0882437029e283b1bf3fd898808db00434
Fr0zT-code <69401786+Fr0zT-code@users.noreply.github.com>
2021-04-10 15:56:12 +0200

    First mute command push

--- commands/moderation/mute.js
+++ commands/moderation/mute.js
@@ -1,17 +1,33 @@
-//const redis = require('../../redis')
+const { MessageEmbed, Discord, Message} = require('discord.js')
 
-//const { MessageEmbed } = require('discord.js')
+module.exports = {
+    commands: 'mute',
+    description: 'Mutes a user',
+    expectedArgs: '<usermention> <duration>',
+    permissionError: 'You do not have permission to use this command',
+    minArgs: 2,
+    maxArgs: 90,
+    callback: (message, arguments, text) => {
+        const target = message.mentions.users.first()
+        let mainRole = message.guild.roles.chache.find(role => role.name === 'Member', 'Owner', 'Admin', 'MH Staff', 'Sr. Developer', 'Developer', 'Jr. Developer', 'Bot Developer', 'Server Owner', 'Minecraft Skript', 'Java', 'JavaScript', 'Python', 'C')
+        let muteRole = message.guild.roles.chache.find(role => role.name === 'Muted')
 
-//module.exports = {
-//    commands: 'mute',
-//    description: 'Mutes a user',
-//    expectedArgs: '<usermention> <duration> <duration_type> <reason>',
-//    permissionError: 'You do not have permission to use this command',
-//    minArgs: 4,
-//    maxArgs: 90,
-//    callback: (message, arguments, text) => {
-//
-//        const split = content.split(' ')
-//    },
-//    requiredRoles: ['Sr. Developer'],
-//}
+        let memberTarget = message.guild.members.cache.get(target.id)
+
+        memberTarget.roles.remove(mainRole.id)
+        memberTarget.roles.add(muteRole.id)
+        const icon = message.guild.iconURL()
+        const embed = new MessageEmbed()
+        .setTitle('User muted')
+        .setThumbnail(icon)
+        .addField(`Moderator`, `${message.author}`)
+        .addField('Victim', `${target}`)
+        .setTimestamp()
+        .setColor('RED')
+        .setAuthor(`${message.guild.name}`, icon)
+        message.channel.send(embed)
+        
+
+    },
+
+}
